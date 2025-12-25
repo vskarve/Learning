4be48b26c5990da593a19a5ce24f81eee80449dc
vskarve <viktor.skarve@gmail.com>
2025-12-25 18:13:01 +0100

    Getting Ipv4 address

--- socket_network/localtest1/server.py
+++ socket_network/localtest1/server.py
@@ -1,2 +1,11 @@
 import socket
 import threading
+
+PORT = 5050
+
+#Ipv4 adress (local adress on router system, wont be reachable outside router)
+#SERVER = "172.29.198.79" (an example, but to get for every computer run below)
+#To get the local on router add "local" otherwise it will give you a adres on local on the computer (127...)
+SERVER = socket.gethostbyname(socket.gethostname() + ".local")
+print(SERVER)
+print(socket.gethostname())
